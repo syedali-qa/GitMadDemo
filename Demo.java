ae7f6fb33383cce6a20204cd3a8ce435dc0c407b
syedali-qa <syedalitest1@gmail.com>
2025-05-26 23:55:44 +0530

    i changed demo java file

--- Demo.java
+++ Demo.java
@@ -1,7 +1,8 @@
-public class Demo{
+public class Demo {
 
-public static void main(){
-System.out.println("This is the demo code");
-}
+	public static void main() {
+		System.out.println("This is the demo code");
+		System.out.println("This is the new added code");
+	}
 
 }
